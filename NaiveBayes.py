9cf78cd0d68c0c4c2601a5535b5b39a196dedd19
Halil Efe Cakmak <159819100+Halil-Efe-Cakmak@users.noreply.github.com>
2024-03-14 19:00:23 +0300

    Add files via upload
    This script employs the Naive Bayes classifier for classification tasks on data from 'veriler.txt'. It begins by loading the dataset and splitting it into training and testing sets. Feature scaling is applied to ensure uniformity across features. The Naive Bayes model, specifically Gaussian Naive Bayes, is then trained on the scaled training data. Subsequently, the model predicts the classes for the test set. Finally, a confusion matrix is computed to evaluate the model's classification performance, providing a detailed analysis of true positive, true negative, false positive, and false negative predictions.

---
+++ NaiveBayes.py
@@ -0,0 +1,35 @@
+#Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Data Install
+veriler = pd.read_csv('veriler.txt')
+
+x = veriler.iloc[:,1:4].values
+y = veriler.iloc[:,4:].values
+
+
+#Data Split to Train And Test
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 0)
+
+#Data scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test)
+
+
+#Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+y_pred = gnb.predict(X_test)
+
+
+#Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
